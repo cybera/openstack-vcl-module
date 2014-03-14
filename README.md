5e4a59d2fe8dd27b8528e074d35d680e48e7fcfd
Cameron Mann <camthemann@gmail.com>
2014-03-14 16:51:38 -0600

    Quick documentation and sql update for completeness. NOT TESTED.

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-This is an updated version of the module that can be found in the [Apache VCL JIRA system](https://issues.apache.org/jira/browse/VCL-590) by Young-Hyun. It may not work with all OpenStack + VCL systems.
+# OpenStack VCL Module
+
+This is an updated version of the module that can be found in the [Apache VCL JIRA system](https://issues.apache.org/jira/browse/VCL-590) by Young Oh. It may not work with all OpenStack + VCL systems. This documentation is currently very incomplete and installation may fail.
+
+## Requirements
 
     $ cpan
     > force install Net::OpenStack::Compute
     > install Try::Tiny
 
-NOTE: This module assumes you are using openstack and NAT and that your managment node is in the same tenant as the entire system.
+NOTE: This module assumes you are using OpenStack and NAT and that your management node is in the same tenant as the entire system.
+
+## Installation
+
+(In progress)
+
+    $ mysql vcl < openstack.sql
+    $ cp openstack.pm <vcl_install_path>/lib/VCL/Module/Provisioning
+
+An existing OpenStack image will need to be inserted manually into the database. Additional entries will also need to be inserted into the OS table depending on which operating systems you use.
--- openstack.sql
+++ openstack.sql
@@ -5,5 +5,27 @@ CREATE TABLE IF NOT EXISTS `openstackComputerMap` (
   UNIQUE KEY (`computerid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE IF NOT EXISTS openstackImageNameMap (
+  openstackimagename varchar(50) NOT NULL DEFAULT '',
+  vclimagename varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (openstackimagename,vclimagename)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 ALTER TABLE `openstackComputerMap`
   ADD CONSTRAINT `openstackComputerMap_ibfk_1` FOREIGN KEY (`computerid`) REFERENCES `computer` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+INSERT INTO module (id, name, prettyname, description, perlpackage)
+  VALUES (28, 'provisioning_openstack', 'OpenStack', '', 'VCL::Module::Provisioning::openstack');
+
+INSERT INTO provisioning (id, name, prettyname, moduleid)
+  VALUES (NULL, 'openstack', 'OpenStack Provisioning', '28');
+
+INSERT INTO provisioningOSinstalltype (provisioningid, OSinstalltypeid)
+  VALUES ('11', '6');
+
+INSERT INTO OSinstalltype (id, name)
+  VALUES (6, 'openstack');
+
+# Change based on your own OS requirements
+INSERT INTO OS (id, name, prettyname, type, installtype, sourcepath, moduleid)
+  VALUES (45, "rhel6openstack", "CentOS 6 OpenStack", "linux", "openstack", "centos6", 5);
